4489c8162c2eb1185d0bfb1e9dbf7db8c083be13
Alfredo Fregoso <afregosoa@gmail.com>
2026-08-25 18:59:59 -0600

    Initial Commit

---
+++ RateMyMovie/RateMyMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateMyMovieApp.swift
+//  RateMyMovie
+//
+//  Created by Alfredo Fregoso on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct RateMyMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RateMyMovieTests/RateMyMovieTests.swift
@@ -0,0 +1,19 @@
+//
+//  RateMyMovieTests.swift
+//  RateMyMovieTests
+//
+//  Created by Alfredo Fregoso on 25/08/26.
+//
+
+import Testing
+@testable import RateMyMovie
+
+struct RateMyMovieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
